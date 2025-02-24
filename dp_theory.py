7a7f37aeb689e8a3dc1da886ea4716523586024d
Abulkosim <abulkasimkattaev@gmail.com>
2025-02-24 21:38:31 +0500

    DP Theory: longest common subsequence

--- dp_theory.py
+++ dp_theory.py
@@ -32,3 +32,49 @@ def fib_memo(n, memo=None):
 
 
 print(fib_memo(10))
+
+'''
+Longest common subsequence: 
+'''
+
+def lcs_memo(X, Y, i=None, j=None, memo=None):
+    """
+    Returns the length of the LCS of X and Y using top-down memoization.
+    i, j represent the current indices we are considering in X, Y respectively.
+    """
+    if memo is None:
+        memo = {}
+    if i is None:
+        i = len(X)
+    if j is None:
+        j = len(Y)
+
+    # If already in memo, return it
+    if (i, j) in memo:
+        return memo[(i, j)]
+
+    # Base case: if one string is empty
+    if i == 0 or j == 0:
+        memo[(i, j)] = 0
+        return 0
+
+    # If the last characters match
+    if X[i-1] == Y[j-1]:
+        memo[(i, j)] = 1 + lcs_memo(X, Y, i-1, j-1, memo)
+    else:
+        # If they don't match
+        memo[(i, j)] = max(
+            lcs_memo(X, Y, i-1, j, memo),
+            lcs_memo(X, Y, i, j-1, memo)
+        )
+
+    return memo[(i, j)]
+
+
+# Example usage:
+X = "ABCDGH"
+Y = "AEDFHR"
+print(lcs_memo(X, Y))  # 3 (LCS is "ADH")
+ 
+ 
+ 
